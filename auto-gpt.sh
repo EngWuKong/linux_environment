5b42972953faa555ff7d2a8cc462286d1ac64b94
an lao sun <129148097+anlaosun@users.noreply.github.com>
2023-04-21 01:16:12 +0800

    Update and rename auto-gpt to auto-gpt.sh

--- auto-gpt.sh
+++ auto-gpt.sh
@@ -5,3 +5,8 @@ apt-get install python3-pip
 git clone https://github.com/Significant-Gravitas/Auto-GPT.git
 cd Auto-GPT
 pip install -r requirements.txt
+
+
+git clone https://github.com/reworkd/AgentGPT.git
+cd AgentGPT
+npm install
