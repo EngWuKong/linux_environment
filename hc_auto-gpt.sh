5b9156e32040919f84259424ce311b756229445f
xiaoheixiaocheng <xiaoheixiaocheng@gmail.com>
2023-04-24 19:04:33 +0800

    update gpt

--- hc_auto-gpt.sh
+++ hc_auto-gpt.sh
@@ -1,12 +1,15 @@
-apt-get update
-apt-get install python3-pip
+function hc_autogpt_install(){
+    apt-get update
+    apt-get install python3-pip
 
 
-git clone https://github.com/Significant-Gravitas/Auto-GPT.git
-cd Auto-GPT
-pip install -r requirements.txt
+    git clone https://github.com/Significant-Gravitas/Auto-GPT.git
+    cd Auto-GPT
+    pip install -r requirements.txt
 
 
-git clone https://github.com/reworkd/AgentGPT.git
-cd AgentGPT
-npm install
+    git clone https://github.com/reworkd/AgentGPT.git
+    cd AgentGPT
+    npm install
+
+}
